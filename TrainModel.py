ed980ba904f1438e60088bcc6d16fb095c9e7a2f
Whallie <phuriinut@gmail.com>
2025-10-14 14:15:11 +0700

    initial commit

---
+++ TrainModel.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from pathlib import Path
+import pandas as pd
+
+DATA_PATH = Path(__file__).parent / "Data" / "Customer-Churn-Records.csv"
+df = pd.read_csv(DATA_PATH)
+print(df.head())
